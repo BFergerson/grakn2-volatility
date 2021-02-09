6204f96df6135ae0c42df205ec650f467cc4ce6f
Brandon Fergerson <bfergerson@apache.org>
2021-02-08 22:38:47 -0500

    two batch

--- src/test/java/TestMatchVolatility.java
+++ src/test/java/TestMatchVolatility.java
@@ -22,12 +22,13 @@ public class TestMatchVolatility {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         context.reset();
 
-        //connect to grakn
         var graknHost = "localhost";
         var graknPort = 1729;
         var graknKeyspace = "grakn";
 
-        for (int i = 0; i < 100; i++) {
+        //first batch
+        for (int i = 0; i < 10; i++) {
+            //connect to grakn
             GraknClient graknClient;
             try (var t = registry.timer("setup").time()) {
                 graknClient = GraknClient.core(graknHost + ":" + graknPort);
@@ -47,7 +48,50 @@ public class TestMatchVolatility {
 
             //read data
             GraknClient.Session dataSession;
-            try (var t = registry.timer("readData-2").time()) {
+            try (var t = registry.timer("readData-withoutRel").time()) {
+                dataSession = graknClient.session(graknKeyspace, GraknClient.Session.Type.DATA);
+                try (var readTx = dataSession.transaction(GraknClient.Transaction.Type.READ)) {
+                    var results = readTx.query().match((GraqlMatch.Aggregate) parseQuery(
+                            "match\n" +
+                                    "$function isa SourceArtifact;\n" +
+//                                    "(is_parent: $function, is_child: $functionName);" +
+                                    "($functionName) isa IDENTIFIER;" +
+                                    "$functionName has token \"main\";\n" +
+                                    "get $function; count;")).get();
+                    System.out.println("Results: " + results);
+                }
+            }
+
+            //clean up
+            dataSession.close();
+            graknClient.close();
+
+            reporter.report();
+        }
+
+        //second batch
+        for (int i = 0; i < 10; i++) {
+            //connect to grakn
+            GraknClient graknClient;
+            try (var t = registry.timer("setup").time()) {
+                graknClient = GraknClient.core(graknHost + ":" + graknPort);
+                if (graknClient.databases().contains(graknKeyspace)) graknClient.databases().delete(graknKeyspace);
+                graknClient.databases().create(graknKeyspace);
+            }
+
+            //save schema
+            try (var t = registry.timer("saveSchema").time()) {
+                var schemaSession = graknClient.session(graknKeyspace, GraknClient.Session.Type.SCHEMA);
+                var tx = schemaSession.transaction(GraknClient.Transaction.Type.WRITE);
+                tx.query().define(parseQuery(Resources.toString(Resources.getResource("schema_bigger.gql"), StandardCharsets.UTF_8)));
+                tx.commit();
+                tx.close();
+                schemaSession.close();
+            }
+
+            //read data
+            GraknClient.Session dataSession;
+            try (var t = registry.timer("readData-withRel").time()) {
                 dataSession = graknClient.session(graknKeyspace, GraknClient.Session.Type.DATA);
                 try (var readTx = dataSession.transaction(GraknClient.Transaction.Type.READ)) {
                     var results = readTx.query().match((GraqlMatch.Aggregate) parseQuery(
